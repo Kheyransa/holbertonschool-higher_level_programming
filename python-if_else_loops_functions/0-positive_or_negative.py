cc2c708806b4c56b2936e170f12810758109a684
Kheyransa <xeyransaqorcuyeva@gmail.com>
2026-04-03 18:02:24 +0000

    Add type check and fix PEP8 for 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
-if number>0:
+
+if type(number) != int:
+    print(f"{number} is wrong type")
+elif number > 0:
     print(f"{number} is positive")
-elif number==0:
+elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
