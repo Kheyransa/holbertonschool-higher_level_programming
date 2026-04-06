99049f64122623a849e7329d451e0e09fbe1f90c
Kheyransa <xeyransaqorcuyeva@gmail.com>
2026-04-06 18:52:08 +0000

    Fixing pycodestyle for Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
+"""
+Roman to Integer module
+"""
+
+
 def roman_to_int(roman_string):
+    """
+    Converts a Roman numeral to an integer
+    """
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
     roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
     total = 0
-    length = len(roman_string)
-    for i in range(length):
-        current_val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < length and roman_dict.get(roman_string[i+1], 0) > current_val:
-            total -= current_val
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman_dict.get(char, 0)
+        if current_value >= prev_value:
+            total += current_value
         else:
-            total += current_val
+            total -= current_value
+        prev_value = current_value
+
     return total
