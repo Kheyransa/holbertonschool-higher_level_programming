5ca5082fe6c80834ea81897fb9eec2b2c5ee889a
Kheyransa <xeyransaqorcuyeva@gmail.com>
2026-04-18 11:43:31 +0000

    Task 11: Implement __str__ for Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains the Square class that inherits from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A representation of a square using Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with size.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns the printable string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
